2adbb8adad17de56079c9920c5d1eacf3854f6d8
erichua <erichua@webank.com>
2017-05-31 03:56:18 +0800

    add release scripts

---
+++ scripts/publish-npm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# if [ $# -lt 1 ];then
+# echo 'Your should input your version numver'
+# exit
+# fi
+
+git add -A
+git commit
+
+cd lib
+version=v$1
+git tag version
+npm publish --access public --verbose
+git push --tag
