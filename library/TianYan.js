8f99b5234cd7dfe6019d3c4ad7dbfbbd4be546ed
yatesmiao <yatesmiao@webank.com>
2019-06-27 16:10:51 +0800

    Fix 单击时的判断问题

--- library/TianYan.js
+++ library/TianYan.js
@@ -15,6 +15,8 @@ import React, { Component } from 'react'
 import Dashboard from './Dashboard'
 import Console from './plugins/console/Console'
 
+const { width, height } = Dimensions.get("window");
+
 export default class TianYan extends Component {
   static propTypes = {
     options: React.PropTypes.object.isRequired
@@ -27,10 +29,6 @@ export default class TianYan extends Component {
     iconSize: 50
   }
 
-  state = {
-    showDashboard: true
-  }
-
   constructor (props) {
     super(props)
     const self = this
@@ -38,56 +36,79 @@ export default class TianYan extends Component {
     Dashboard.setup()
 
     this._toggleDashboard = this._toggleDashboard.bind(this)
-    const {width} = Dimensions.get('window')
     this.state = {
       pan: new Animated.ValueXY({
         x: width - this.size.iconSize,
         y: 300
       }),
-      expendAnim: new Animated.Value(0)
+      expendAnim: new Animated.Value(0),
+      showDashboard: false
     }
+
     this.panResponder = PanResponder.create({
       onMoveShouldSetPanResponder: () => true,
-      onMoveShouldSetPanResponderCapture: (evt, gestureState) => {
-        return gestureState.dx < 1 || gestureState.dy < 1
-      },
-      onStartShouldSetPanResponderCapture: () => false,
-      onPanResponderGrant: (e, gestureState) => {
-        this.state.pan.setOffset({
-          x: self.state.pan.x._value,
-          y: self.state.pan.y._value
-        })
-      },
-      onStartShouldSetPanResponder: (e, gestureState) => {
-        return true
-      },
-      onPanResponderMove: Animated.event([null, {
-        dx: self.state.pan.x,
-        dy: self.state.pan.y
-      }]),
-      onPanResponderRelease: (e, gesture) => {
-        console.log(gesture)
-        self.state.pan.flattenOffset()
+      onMoveShouldSetPanResponderCapture: () => true,
+      onPanResponderGrant: this._onPanResponderGrant,
+      onPanResponderMove: this._onPanResponderMove,
+      onPanResponderRelease: this._onPanResponderRelease,
+      onPanResponderTerminate: this._onPanResponderRelease
+    })
+  }
+  componentDidMount () {
+  }
 
-        let toX = 0
-        if ((this.state.pan.x._value + self.size.iconSize / 2) > width / 2) {
-          toX = width - self.size.iconSize
-        }
-
-        Animated.spring(            // Step 1
-          this.state.pan,         // Step 2
-          {
-            toValue: {
-              x: toX,
-              y: self.state.pan.y._value
-            }
-          }     // Step 3
-        ).start(() => {
-        })
-      }
+  _onPanResponderGrant = (evt, gestureState) => {
+    this.time = Date.parse(new Date())
+    this.state.pan.setOffset({
+      x: this.state.pan.x._value,
+      y: this.state.pan.y._value
     })
   }
 
+  _onPanResponderMove = (evt, gestureState) => {
+    Animated.event([
+      null,
+      {
+        dx: this.state.pan.x,
+        dy: this.state.pan.y
+      }
+    ])(evt, gestureState)
+  }
+  _onPanResponderRelease = (evt, gestureState) => {
+    this.state.pan.flattenOffset()
+
+    // 处理y轴
+    const y = this.state.pan.y._value
+    if (y < 10 || y > height - this.size.iconSize - 10) {
+      Animated.spring(this.state.pan.y, {
+        toValue: y < 45 ? 45 : height - this.size.iconSize - 10,
+        duration: 200
+      }).start();
+    }
+
+    // 处理x轴
+    let toX = 0
+    if ((this.state.pan.x._value + this.size.iconSize / 2) > width / 2) {
+      toX = width - this.size.iconSize
+    }
+    Animated.spring(this.state.pan.x, {
+      toValue: gestureState.moveX > width * 0.5 ? width - this.size.iconSize : 0,
+      duration: 200
+    }).start();
+
+    this.lastValueX = this.state.pan.x._value;
+    this.lastValueY = this.state.pan.y._value;
+
+    // 单击
+    let releaseTime = Date.parse(new Date())
+    if (
+      releaseTime - this.time < 50 &&
+      Math.abs(gestureState.dx) < 10 &&
+      Math.abs(gestureState.dy) < 10
+    ) {
+      this._toggleDashboard()
+    }
+  }
   _toggleDashboard () {
     this.setState({
       isAnimationRunning: true
@@ -136,14 +157,6 @@ export default class TianYan extends Component {
               inputRange: [0, 100],
               outputRange: [this.size.iconSize / 2, 0],
             }),
-            top: this.state.expendAnim.interpolate({
-              inputRange: [0, 100],
-              outputRange: [this.state.pan.getLayout().top._value, 0],
-            }),
-            left: this.state.expendAnim.interpolate({
-              inputRange: [0, 100],
-              outputRange: [this.state.pan.getLayout().left._value, 0],
-            }),
             width: this.state.expendAnim.interpolate({
               inputRange: [0, 100],
               outputRange: [this.size.iconSize, Dimensions.get('window').width],
@@ -151,33 +164,41 @@ export default class TianYan extends Component {
             height: this.state.expendAnim.interpolate({
               inputRange: [0, 100],
               outputRange: [this.size.iconSize, Dimensions.get('window').height],
+            }),
+            top: this.state.expendAnim.interpolate({
+              inputRange: [0, 100],
+              outputRange: [this.state.pan.getLayout().top._value, 0],
+            }),
+            left: this.state.expendAnim.interpolate({
+              inputRange: [0, 100],
+              outputRange: [this.state.pan.getLayout().left._value, 0],
             })
           } : {}
         ]}>
 
 
         {this.state.showDashboard ? <Animated.View
-          style={{
-            position: 'absolute',
-            alignSelf: 'stretch',
-            opacity: this.state.expendAnim.interpolate({
-              inputRange: [0, 40, 100],
-              outputRange: [0, 0, 1],
-            }),
-            top: 0,
-            bottom: 0,
-            left: 0,
-            right: 0,
-            flex: 1,
-            flexDirection: 'column'
-          }}>
-          <Dashboard onPressClose={this._toggleDashboard} />
-        </Animated.View> :
+            style={{
+              position: 'absolute',
+              alignSelf: 'stretch',
+              opacity: this.state.expendAnim.interpolate({
+                inputRange: [0, 40, 100],
+                outputRange: [0, 0, 1],
+              }),
+              top: 0,
+              bottom: 0,
+              left: 0,
+              right: 0,
+              flex: 1,
+              flexDirection: 'column'
+            }}>
+            <Dashboard onPressClose={() => { this._toggleDashboard() }} />
+          </Animated.View> :
           <TouchableOpacity
             style={{
               borderRadius: this.size.iconSize / 2,
             }}
-            onPress={this._toggleDashboard}>
+            onPress={() => {}}>
             <Animated.Image style={{
               opacity: this.state.expendAnim.interpolate({
                 inputRange: [0, 40, 100],
