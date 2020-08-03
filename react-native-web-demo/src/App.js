7dc0b5d904398b363e3f1418468d8706b1667459
lhtin <lehuading@qq.com>
2020-08-03 16:37:15 +0800

    add: more log's type and opcions

--- react-native-web-demo/src/App.js
+++ react-native-web-demo/src/App.js
@@ -7,14 +7,21 @@ import TianYan from './library'
 
 class App extends React.Component {
   componentDidMount () {
-    console.log('a normal log')
-    console.error('a error log')
-    console.warn('a warn log')
-    console.error('a error log')
-    console.log('a normal log')
-    console.error('a error log')
-    console.warn('a warn log')
-    fetch('https://static.geetest.com/static/js/fullpage.8.9.6.js')
+    const data = {
+      name: 'wt-console',
+      version: 'x.x.x',
+      desc: 'A lightweight, extendable debug tool for React Native'
+    }
+    console.log('a normal log', data)
+    console.error('a error log', data)
+    console.warn('a warn log', data)
+    console.log('%ca normal log with custom style', 'color: crimson;')
+    console.groupCollapsed('a group logs')
+    console.log('item 1')
+    console.warn('item 2')
+    console.error('item 3')
+    console.groupEnd()
+    fetch('https://reactjs.org/docs/getting-started.html')
     fetch('https://static.geetest.com/static/js/fullpage.8.9.600.js')
     fetch('https://static.geetest.com/static/wind/sprite2x.1.5.8.png')
     fetch('https://static.geetest.com/static/js/fullpage.8.9.6.js')
@@ -25,7 +32,8 @@ class App extends React.Component {
       <View>
         <Text>wt-console demo</Text>
         <TianYan
-          options={{
+          consoleOptions={{
+            showTimestamp: true,
             ignoreRedBox: true,
             ignoreYellowBox: true
           }}
@@ -35,4 +43,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default App
